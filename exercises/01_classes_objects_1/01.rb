d419a5ed6cc48c67bb6e7bbe7e8b6de269806660
oscos <cortes.oscar@outlook.com>
2021-07-14 04:45:41 +0000

    adding ex01.rb

---
+++ exercises/01_classes_objects_1/01.rb
@@ -0,0 +1,14 @@
+=begin
+Launch School: RB120 - Object Oriented Programming 
+Exercise Set:  OO Basics: Classes and Objects 1
+ExerciseName: [Find The Class](https://launchschool.com/exercises/d44de169)
+FileName: ex01.rb
+Answered On: 07/13/2021
+=end
+
+# Update the following code so that, instead of printing the values, each 
+# statement prints the name of the class to which it belongs.
+
+puts "Hello".class
+puts 5.class
+puts [1, 2, 3].class 
---
+++ exercises/01_classes_objects_1/ex.rb
@@ -0,0 +1,7 @@
+=begin
+Launch School: RB120 - Object Oriented Programming 
+Exercise Set:  OO Basics: Classes and Objects 1
+ExerciseName: [Find The Class](https://launchschool.com/exercises/d44de169)
+FileName: ex01.rb
+Answered On: 07/13/2021
+=end
