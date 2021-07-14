72e323f43f80bcfa23587409dda6f15b8d72bc70
oscos <cortes.oscar@outlook.com>
2021-07-14 06:04:14 +0000

    set 1 exercises

--- exercises/01_classes_objects_1/01.rb
+++ exercises/01_classes_objects_1/01.rb
@@ -1,9 +1,10 @@
 =begin
-Launch School: RB120 - Object Oriented Programming 
-Exercise Set:  OO Basics: Classes and Objects 1
-ExerciseName: [Find The Class](https://launchschool.com/exercises/d44de169)
-FileName: ex01.rb
-Answered On: 07/13/2021
+School: Launch School  
+Course: RB120 - Object Oriented Programming  
+Exercise Set:  OO Basics: Classes and Objects 1  
+ExerciseName: [Find the Class](https://launchschool.com/exercises/d44de169)  
+FileName: ex01.md  
+Answered On: 07/13/2021  
 =end
 
 # Update the following code so that, instead of printing the values, each 
